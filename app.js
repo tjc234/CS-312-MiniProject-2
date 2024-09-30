c31600f8233b0c2cb0f6329a1b1cd86687d631f1
tjc234 <tjc234@nau.edu>
2024-09-30 12:47:17 -0700

    add number of joke functionality

--- app.js
+++ app.js
@@ -17,22 +17,27 @@ app.set( 'view engine', 'ejs' );
 // routing to homepage (GET)
 app.get( '/', ( req, res ) => {
     // render index.ejs with joke as null
-    res.render( 'index', { joke: null } );
+    res.render( 'index', { jokes: null } );
 }); 
 
 // form submisison route (POST)
 app.post( '/', async ( req, res ) => {
+    const jokes = [];
+    const numJokes = parseInt( req.body.numJokes );
+
     try {
-    // get joke from API
-    const response = await axios.get( 'https://official-joke-api.appspot.com/random_joke' );
-    const joke = response.data;
+    // fetch requested number of jokes
+    for( let i = 0; i < numJokes; i++ ) {
+        const response = await axios.get( 'https://official-joke-api.appspot.com/random_joke' ) ;
+        jokes.push( response.data );
+    }
 
     // render index.ejs with joke
-    res.render( 'index', { joke: joke } );
+    res.render( 'index', { jokes: jokes } );
     } catch( error ) {
         // handle error
         console.error( 'Error fetching joke(s):', error );
-        res.render( 'index', { joke: null } );
+        res.render( 'index', { jokes: null } );
     }
 });
 
