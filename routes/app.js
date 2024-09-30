556ec99af90439994275f0bdda7a42e294ea3b65
tjc234 <tjc234@nau.edu>
2024-09-30 11:58:06 -0700

    initial server implementation

--- routes/app.js
+++ routes/app.js
@@ -16,9 +16,21 @@ app.set( 'view engine', 'ejs' );
 
 // routing to homepage (GET)
 app.get( '/', ( req, res ) => {
+    // render index.ejs with joke as null
     res.render( 'index', { joke: null } );
+}); 
+
+// form submisison route (POST)
+app.post( '/', async ( req, res ) => {
+    // get joke from API
+    const response = await axios.get( 'https://official-joke-api.appspot.com/random_joke' );
+    joke = response.data;
+
+    // render index.ejs with joke
+    res.render( 'index', { joke: joke } );
 });
 
-// form submisison route
-
 // start server
+app.listen( port, () => {
+    console.log( 'Server started on port: ${port}' );
+});
