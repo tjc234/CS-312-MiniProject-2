621d36e0a12733001a4196a2fc7c1dc2cbcf25ca
tjc234 <tjc234@nau.edu>
2024-09-30 11:51:21 -0700

    app setup and homepage route

--- routes/app.js
+++ routes/app.js
@@ -1,12 +1,23 @@
 // import libraries
+import express from 'express';
+import bodyParser from 'body-parser';
+import axios from 'axios';
 
 // initialze app
+const app = express();
+const port = 3000;
 
 // set static folder and body parser
+app.use( express.static( 'public') );
+app.use( bodyParser.urlencoded( { extended: true } ) );
 
 // set view engine
+app.set( 'view engine', 'ejs' );
 
-// routing to homepage
+// routing to homepage (GET)
+app.get( '/', ( req, res ) => {
+    res.render( 'index', { joke: null } );
+});
 
 // form submisison route
 
