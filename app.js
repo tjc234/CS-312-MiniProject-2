fca06a1a1c46e2e893a1eaac99d9ea4948fb26d2
tjc234 <tjc234@nau.edu>
2024-09-30 18:17:46 -0700

    comments on apps.js

--- app.js
+++ app.js
@@ -22,26 +22,36 @@ app.get( '/', ( req, res ) => {
 
 // form submisison route (POST)
 app.post( '/', async ( req, res ) => {
+    // initialize variables
     const jokes = [];
     const numJokes = parseInt( req.body.numJokes );
 
+    // start try block
     try {
     // fetch requested number of jokes
     for( let i = 0; i < numJokes; i++ ) {
+        // fetch joke from API
         const response = await axios.get( 'https://official-joke-api.appspot.com/random_joke' ) ;
+
+        // add joke to jokes array
         jokes.push( response.data );
     }
 
     // render index.ejs with joke
     res.render( 'index', { jokes: jokes } );
+
+    // catch error block
     } catch( error ) {
         // handle error
         console.error( 'Error fetching joke(s):', error );
+
+        // render index with joke as null
         res.render( 'index', { jokes: null } );
     }
 });
 
 // start server
 app.listen( port, () => {
+    // output message to console
     console.log( `Server started on port: ${port}` );
 });
